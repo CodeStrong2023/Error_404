320bd3a2e7b9752ea9a807be5ed4c05a385421b1
Ailín <129910296+Ailinchu@users.noreply.github.com>
2023-09-27 01:04:39 -0300

    Completando ejercicios de Python

---
+++ AilínMorales/Java/Ejerciciodeconcatenacion/src/ejerciciodeconcatenacion/Ejerciciodeconcatenacion1.java
@@ -0,0 +1,16 @@
+package ejerciciodeconcatenacion;
+
+/**
+ *
+ * @author Ailín
+ */
+public class Ejerciciodeconcatenacion1 {
+
+    public static void main(String[] args) {
+        var usuario = "Osvaldo";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+
+    }
+}
---
+++ AilínMorales/Java/Ejerciciodeconcatenacion2/src/ejerciciodeconcatenacion2/Ejerciciodeconcatenacion2.java
@@ -0,0 +1,16 @@
+
+package ejerciciodeconcatenacion2;
+
+/**
+ *
+ * @author Ailín
+ */
+public class Ejerciciodeconcatenacion2 {
+
+    public static void main(String[] args) {
+    var a = 8;
+    var b = 4;
+        System.out.println (a + b);
+    }
+    
+}
