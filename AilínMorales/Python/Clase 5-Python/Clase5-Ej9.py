79b3b83a46d33aad497e6c98cdb58d282d23aabb
Ailín <129910296+Ailinchu@users.noreply.github.com>
2023-10-24 20:19:34 -0300

    Ejercicios de Pyhton hasta la clase 7

---
+++ AilínMorales/Python/Clase 5-Python/Clase5-Ej9.py
@@ -0,0 +1,10 @@
+"""
+
+Ejercicio 9: Mostrar una frase sin espacios y contar su longitud
+Hacer un programa donde el usuario ingrese una frase, se le devolvera la misma frase pero sin espacios en blanco, y además un contador de cuántos caracteres tiene la frase (sin contar
+los espacios en blanco)
+Ejemplo: frase = vivir por siempre en paz
+         frase final = vivirporsiempreenpaz
+         Nº de caracteres = 20
+
+"""
