9cde87cd6620a00a503301786cc535b2d28b998b
Gonzalo <gonzaloromero2207@gmail.com>
2023-11-02 01:31:37 -0300

    Completando trabajos faltantes de la carpeta

---
+++ GonzaloRomero/JavaScript/clase 6/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,17 @@
+var nombre = 'Carlos';
+var apellido = ' Montes';
+var nombreCompleto = nombre+' '+apellido;
+console.log(nombreCompleto);
+var nombreCompleto2 = 'Gonzalo'+' '+'Romero';
+console.log(nombreCompleto2);
+var juntos = nombre + 219; //Lee de izq a der siguiendo la cadena lee el numero como str
+console.log(juntos);
+juntos = nombre + 78 + 17; //Aqui se puede diferenciar a traves de los parentesis
+console.log(juntos);
+juntos = nombre + (78 + 17)
+console.log(juntos)
+juntos = 78 + 17 + nombre;
+console.log(juntos);
+
+nombre += apellido; //Tercera Concatenacion usando el operador simplificado
+console.log(nombre);
