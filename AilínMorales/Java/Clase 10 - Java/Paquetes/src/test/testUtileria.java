377718cf69550c98c87b856f4ade60851d126ac8
Ailín <129910296+Ailinchu@users.noreply.github.com>
2023-11-02 01:31:50 -0300

    Completando carpetas de Java, Python y JS

---
+++ AilínMorales/Java/Clase 10 - Java/Paquetes/src/test/testUtileria.java
@@ -0,0 +1,13 @@
+
+package test;
+//import ar.com.codesystem.Utileria.*;
+//import ar.com.codesystem.Utileria;
+
+//import static ar.com.codesystem.Utileria.imprimir;//solo aplica para emtodos estaticos
+public class testUtileria {
+    public static void main(String[] args) {
+ //       Utileria.imprimir("Saludos a los chicos del grupo");
+ //       imprimir("Terminando");
+          ar.com.codesystem.Utileria.imprimir("Ahora si termina, aunque no sea recomendable");
+    }
+}
